0f8571c5dbf350679f21a5a2a9ac57eef2038169
Alexander <alexmezhenskyi@gmail.com>
2018-10-13 09:14:06 +0300

    added self-invokedfunction with factory logic, need to do

---
+++ js/test.js
@@ -0,0 +1,14 @@
+const arrW = 6;
+const arrH = 4;
+const corners = [0, 200, 50, 100];
+
+(function() {
+	const colors = [];
+
+	for (let i = 0; i < arrH; i++) {
+		colors[i] = [];
+		for (let j = 0; j < arr; j++) {
+
+		}
+	}
+})();
