60570d464b6872dc952a1d1e0462f49958eb9675
Alexander <alexmezhenskyi@gmail.com>
2018-10-13 09:45:55 +0300

    need to do

--- js/test.js
+++ js/test.js
@@ -2,8 +2,14 @@ const arrW = 6;
 const arrH = 4;
 const corners = [0, 200, 50, 100];
 
+function step(from, to, size) {
+	return (to - from) / (size - 1);
+}
+
 (function() {
 	const colors = [];
+	const left = [];
+	const right = [];
 
 	for (let i = 0; i < arrH; i++) {
 		colors[i] = [];
